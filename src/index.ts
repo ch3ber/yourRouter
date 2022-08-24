45c36e680be74e3f6ce698443323e1101623eb08
Eber Alejo <eber4202@hotmail.com>
2022-08-23 21:21:33 -0500

    [REFACTOR] package.json and dev env

--- src/index.ts
+++ src/index.ts
@@ -1,91 +1,90 @@
-// imports
+import { Route } from '@models/route.model'
+import { AddRouteCallback, RouterConfig } from '@models/router.model'
 import { Template } from '@models/template.model'
-import { Router } from '@router/index'
+import { GetRouteInfo } from '@utils/getRouteInfo'
+import { renderInHtmlNode } from '@utils/renderInHtmlNode'
+import { RouteManager } from '@utils/routeManager'
 
-Router.createInstance({
-  path404: '/notFound',
-  renderId: '#app'
-})
-const router = Router.getInstance()
+const getRouteInfo = new GetRouteInfo()
+const routeManager = RouteManager.getInstance()
 
-const Menu = (): Template => {
-  const view = `
-    <ul>
-      <li><a href="/#/">Go to Home</a></li>
-      <li><a href="/#/test">Go to Test</a></li>
-      <li><a href="/#/test/test">Go to Test</a></li>
-      <li><a href="/#/product/123">Go to dynamic route</a></li>
-      <li><a href="/#/newpost/321">Go to dynamic route similar</a></li>
-      <li><a href="/#/post/New Post">Go to dynamic route</a></li>
-      <li><a href="/#/api/products/categories/small/20398475">Go to extend dynamic route</a></li>
-      <li><a href="/#/api/products">Go to json</a></li>
-      <li><a href="/#/api/product/:id">Go to dynamic json</a></li>
-    </ul>
-  `
-  return view
+export class Router {
+  // eslint-disable-next-line no-use-before-define
+  private static instance: Router
+  private renderId: string
+  private path404: string
+
+  private constructor (config: RouterConfig) {
+    this.path404 = config.path404
+    this.renderId = config.renderId ?? ''
+  }
+
+  /*
+    singleton method
+  */
+  public static createInstance (config: RouterConfig): void {
+    if (config.path404 === undefined) {
+      throw new Error('Need specific 404 HTTP Error path in Router config. To fix use: { path404: \'YOUR PATH\' }')
+    }
+    Router.instance = new Router(config)
+  }
+
+  /*
+    singleton method
+  */
+  public static getInstance (): Router {
+    if (!Router.instance) {
+      throw new Error('Need create Router instance: posible fix "Router.createInstance()"')
+    }
+    return Router.instance
+  }
+
+  private async mount (): Promise<void> {
+    // set route to /#/
+    window.location.hash = '/'
+
+    // mount router
+    window.addEventListener('hashchange', async (event: HashChangeEvent) => {
+      event.preventDefault()
+      const path = getRouteInfo.path()
+      if (this.renderId === '') return
+      await this.renderRoute(path)
+    })
+
+    // render indexRoute
+    if (this.renderId === '') return
+    await this.renderRoute('/')
+  }
+
+  /*
+    render actual route into html node (renderId)
+  */
+  private async renderRoute (path: string) {
+    if (!getRouteInfo.isValidRoute(path)) {
+      this.redyrectTo(this.path404)
+      return
+    }
+
+    const route = routeManager.find(path)
+    const routeCallback = route?.callback as () => unknown
+    await renderInHtmlNode(routeCallback() as () => Template, this.renderId)
+  }
+
+  redyrectTo (to: Route['path']): void {
+    window.location.hash = to
+  }
+
+  /*
+    add a new route
+  */
+  addRoute (path: string, callback: AddRouteCallback) {
+    if (routeManager.getAllPaths().length === 1) this.mount()
+    const route = { path, callback }
+    routeManager.add(route)
+  }
+
+  getRouteParam (): string {
+    const splitPath = window.location.hash.slice(1).split('/')
+    return splitPath[splitPath.length - 1]
+  }
 }
-
-const DynamicRoute = (): Template => {
-  const param = router.getRouteParam()
-  const view = `<h1>The route param is: ${param}</h1>`
-  return view
-}
-
-const foo = (): Template => {
-  const view = `
-    <h1>Test page addRoute</h1>
-    ${Menu()}
-  `
-  return view
-}
-
-const Home = (): Template => {
-  const view = `
-    <h1>Home</h1>
-    ${Menu()}
-  `
-  return view
-}
-
-router.addRoute('/', () => {
-  // console.log(getRouteInfo.get())
-  return Home
-  // document.querySelector('body')!.innerHTML = Home() as string
-})
-
-router.addRoute('/test', () => {
-  // console.log(getRouteInfo.get())
-  return foo
-  // document.querySelector('body')!.innerHTML = foo() as string
-})
-
-router.addRoute('/test/test', () => {
-  // console.log(getRouteInfo.get())
-  return foo
-  // document.querySelector('body')!.innerHTML = foo() as string
-})
-router.addRoute('/product/:id', () => {
-  // console.log(getRouteInfo.get())
-  return DynamicRoute
-  // document.querySelector('body')!.innerHTML = foo() as string
-})
-router.addRoute('/newpost/:id', () => {
-  // console.log(getRouteInfo.get())
-  return DynamicRoute
-  // document.querySelector('body')!.innerHTML = foo() as string
-})
-router.addRoute('/post/:id', () => {
-  // console.log(getRouteInfo.get())
-  return DynamicRoute
-  // document.querySelector('body')!.innerHTML = foo() as string
-})
-router.addRoute('/api/products/categories/small/:id', () => {
-  // console.log(getRouteInfo.get())
-  return DynamicRoute
-  // document.querySelector('body')!.innerHTML = foo() as string
-})
-router.addRoute('/notFound', () => {
-  // console.log(getRouteInfo.get())
-  return () => '<h1>Error 404</h1>'
-  // document.querySelector('body')!.innerHTML = '<h1>Error 404</h1>'
-})
