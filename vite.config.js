a64af96ecd5fca49140e8c9926f5b6025e9bc6b7
Eber Alejo <eber4202@hotmail.com>
2023-07-14 13:00:31 -0600

    CHORE: fix alias in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import { resolve } from 'path'
 export default defineConfig({
   resolve: {
     alias: {
-      '@/': resolve(__dirname, './src'),
-      '@types': resolve(__dirname, './src/types.d.ts')
+      '@': resolve(__dirname, 'src'),
+      '@types': resolve(__dirname, 'src/types.d.ts')
     }
   },
   test: {
