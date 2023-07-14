28b635fe7bc1f7a7a83c125270704ac8d506bcf8
Eber Alejo <eber4202@hotmail.com>
2023-07-14 01:05:54 -0600

    FIX: router test

--- __test__/router/infrastructure/router.test.ts
+++ __test__/router/infrastructure/router.test.ts
@@ -1,11 +1,11 @@
-import { Router } from '@/router'
+import { Router } from '@/router/infrastructure/router'
 import { describe, expect, it } from 'vitest'
 
 describe('Router test', () => {
   describe('createInstance method', () => {
     it('Trhow an error if path404 is not present', () => {
       const testFn = () => {
-        Router.createInstance({})
+        Router.create({})
       }
       const errorMsg = 'Need specific 404 HTTP Error path in Router config. To fix use: { path404: \'YOUR PATH\' }'
 
