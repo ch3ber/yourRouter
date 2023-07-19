abfca1730475e7ebd0c4b898d0bc7e897132bc8a
Eber Alejo <eber4202@hotmail.com>
2023-07-19 02:20:23 -0600

    FIX: add invalid routes

--- __test__/router/application/AddRoute.test.ts
+++ __test__/router/application/AddRoute.test.ts
@@ -1,7 +1,54 @@
-import { describe, expect, it } from 'vitest'
+import { AddRoute } from '@/router/application/AddRoute'
+import { RouteManager } from '@/routes/domain/routeManager'
+import { RoutePath } from '@/types'
+import { afterEach, describe, expect, it } from 'vitest'
 
 describe('AddRoute class', () => {
-  it('should be true', () => {
-    expect(true).toBe(true)
+  afterEach(() => {
+    RouteManager.getInstance().reset()
+  })
+
+  it("should throw an error if the route don't start with /", () => {
+    const addRoute = new AddRoute()
+
+    const testFn = () => {
+      const routeToTest: RoutePath = 'foo/bar'
+      addRoute.add(routeToTest, () => {})
+    }
+
+    const errorMsg = 'Error adding a route. All paths need start with "/". Example: /test/route or /dynamic/route/:id'
+
+    expect(testFn).toThrowError(errorMsg)
+  })
+
+  it('should throw an error if the user try to add an existing route', () => {
+    const addRoute = new AddRoute()
+
+    const routeToTest: RoutePath = '/'
+
+    const testFn = () => {
+      addRoute.add(routeToTest, () => {})
+      addRoute.add(routeToTest, () => {})
+    }
+
+    const errorMsg = `Error adding a route. The router don't allow duplicated routes. The route "${routeToTest}" is an existing route.`
+
+    expect(testFn).toThrowError(errorMsg)
+  })
+
+  it("should throw an error if the route don't match with a route regex", () => {
+    const addRoute = new AddRoute()
+
+    const routesToTest: RoutePath[] = ['/:::', '/::', '/]', '//', '/:id:', '/foo/', '/foo/:-', '/:-', '/-', '/:', '/-:']
+
+    for (const routeToTest of routesToTest) {
+      const testFn = () => {
+        addRoute.add(routeToTest, () => {})
+      }
+
+      const errorMsg = `Error adding a route. The route "${routeToTest}" is not a valid route.`
+
+      expect(testFn).toThrowError(errorMsg)
+    }
   })
 })
